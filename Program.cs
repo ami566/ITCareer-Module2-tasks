a9464099c048c84432376feb4e0d8a858be3ecf5
ami566 <47269677+ami566@users.noreply.github.com>
2019-02-04 13:09:20 +0200

    from16to10system

--- Program.cs
+++ Program.cs
@@ -4,19 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace _16system
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var centuries = int.Parse(Console.ReadLine());
-            var years = centuries * 100 ;
-            var days = (int)(years*365.2422);
-            var hours = days*24;
-            var minutes = hours * 60;
-
-            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes", centuries, years,days,hours,minutes);
+            string num1 = Console.ReadLine();
+            var num2 = Convert.ToInt32(num1, 16);
+            Console.WriteLine(num2);
         }
     }
 }
